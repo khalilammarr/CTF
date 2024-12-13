405591bd863643c47741fd4dbd3c4807b931835d
khalilammarr <khalilammar241@gmail.com>
2024-12-13 21:16:15 +0100

    Update script.py

--- GoCipher/invert.py
+++ GoCipher/invert.py
@@ -15,20 +15,20 @@ def brute_force_GoCipher_flag():
             candidate = flag + ch
             print(f"Trying key: {candidate}")
             process = subprocess.Popen(
-                [r'.\gocipher.exe'],  # The executable
-                stdin=subprocess.PIPE,  # Pipe the input to stdin
-                stdout=subprocess.PIPE,  # Capture the output
-                stderr=subprocess.PIPE,  # Capture any errors
-                text=True  # Return output as a string (not bytes)
+                [r'.\gocipher.exe'],  
+                stdin=subprocess.PIPE,  
+                stdout=subprocess.PIPE, 
+                stderr=subprocess.PIPE,  
+                text=True  
             )
             
-            # Pass the candidate as input to inverted.exe and get the output
-            output, error = process.communicate(input=candidate + "\n")  # Send input and get output
+            # Pass the candidate as input to GOcIPHER.exe and get the output
+            output, error = process.communicate(input=candidate + "\n")  
 
             output = output.strip()  # Clean up the output
             if "Congratulations" in output:
                 print(f"Valid flag found so far: {candidate}")
-                flag = candidate  # Update the global flag with the correct character
-                break  # Move to the next character
+                flag = candidate
+                break  
 
 brute_force_GoCipher_flag()
